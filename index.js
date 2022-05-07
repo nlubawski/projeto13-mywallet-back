57d9a8abab57ca2ab2232b978adf60d97b350729
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-05-07 17:30:52 -0300

    feat:created post /sign-in

--- index.js
+++ index.js
@@ -19,10 +19,25 @@ app.post("/sign-up", async (req, res) => {
   const user = req.body;
   const passwordHash = bcrypt.hashSync(user.password, 10);
 
-  // Insira o usuário no banco, criptografando a senha com bcrypt
+  
   try {
     await db.collection("users").insertOne({...user, password: passwordHash});
     res.sendStatus(201);
+  } catch(e) {
+    res.sendStatus(500);
+  }
+
+});
+
+app.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await db.collection("users").findOne({email});
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(401);
+    }
   } catch(e) {
     res.sendStatus(500);
   }
