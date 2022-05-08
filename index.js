54cd7c48c1e6151ee88880c8b97622a3cf597497
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-05-08 18:11:46 -0300

    feat: created get /extrato, that searchs the user extract

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
+import cors from 'cors'
+
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
@@ -12,18 +14,16 @@ mongoClient.connect(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.post("/sign-up", async (req, res) => {
-  //name, email, password
-  //validar senha e confirmacao de senha no front
   const user = req.body;
   const passwordHash = bcrypt.hashSync(user.password, 10);
 
-  
   try {
-    await db.collection("users").insertOne({...user, password: passwordHash});
+    await db.collection("users").insertOne({ ...user, password: passwordHash, extract: [] });
     res.sendStatus(201);
-  } catch(e) {
+  } catch (e) {
     res.sendStatus(500);
   }
 
@@ -32,13 +32,30 @@ app.post("/sign-up", async (req, res) => {
 app.post("/sign-in", async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await db.collection("users").findOne({email});
+    const user = await db.collection("users").findOne({ email });
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.sendStatus(200);
+      res.send(user);
     } else {
       res.sendStatus(401);
     }
-  } catch(e) {
+  } catch (e) {
+    res.sendStatus(500);
+  }
+
+});
+
+app.get("/extrato", async (req, res) => {
+  let {authorization} = req.headers;
+  const passwordToken = authorization.slice(7);
+  try {
+
+    const user = await db.collection("users").findOne({ password:passwordToken });
+    if (user.password === passwordToken) {
+      res.send(user.extract);
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (e) {
     res.sendStatus(500);
   }
 
